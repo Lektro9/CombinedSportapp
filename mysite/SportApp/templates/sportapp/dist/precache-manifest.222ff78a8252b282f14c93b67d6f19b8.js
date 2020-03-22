9de00eca5a6bdc0e77e9dfae3390838cee9d377d
root <root@TheBestNew01-PC.localdomain>
2020-03-22 13:40:30 +0100

    add newest build files

--- mysite/SportApp/templates/sportapp/dist/precache-manifest.222ff78a8252b282f14c93b67d6f19b8.js
+++ mysite/SportApp/templates/sportapp/dist/precache-manifest.222ff78a8252b282f14c93b67d6f19b8.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "793b6264d2f35916e47847f106b3181b",
+    "revision": "5d1ad80c988dc3bef3df597eac6724b8",
     "url": "/index.html"
   },
   {
@@ -8,16 +8,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/manifest.json"
   },
   {
-    "revision": "a6dc4b8efa0a2aeceb9f",
-    "url": "/static/css/app.7ea59caa.css"
+    "revision": "db8e30b69ae5fdbe2827",
+    "url": "/static/css/app.1bb62379.css"
   },
   {
     "revision": "caf366bd74258ff0d58c",
     "url": "/static/css/chunk-vendors.1ea579b2.css"
   },
   {
-    "revision": "a6dc4b8efa0a2aeceb9f",
-    "url": "/static/js/app.7f478c35.js"
+    "revision": "db8e30b69ae5fdbe2827",
+    "url": "/static/js/app.127a868f.js"
   },
   {
     "revision": "caf366bd74258ff0d58c",
