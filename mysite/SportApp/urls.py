c87af777e8384fb19c88cde73f7f9d520bf86808
Lars Kroll <larsk@scoop-software.de>
2020-05-23 22:37:39 +0200

    disable graphiql

--- mysite/SportApp/urls.py
+++ mysite/SportApp/urls.py
@@ -9,7 +9,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('index.html', views.index, name='index'),
-    path("graphql", csrf_exempt(GraphQLView.as_view(graphiql=True))),
+    path("graphql", csrf_exempt(GraphQLView.as_view(graphiql=False))),
     path('manifest.json', (TemplateView.as_view(template_name="manifest.json", content_type='application/json', )), name='manifest.json'),
     path('static/icon192.png', (TemplateView.as_view(template_name="icon192.png", content_type='image/png', )), name='icon192.png'),
     path('service-worker.js', (TemplateView.as_view(template_name="sportapp/dist/service-worker.js", content_type='application/javascript', )), name='service-worker.js'),
